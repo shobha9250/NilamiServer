99bc2d3f53a1937df9ea6bcebafc326be0fec3d6
shobha k <shobha9250@gmail.com>
2022-02-17 23:10:31 +0530

    deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
+
 app.use(express.json());
 app.use(
   express.urlencoded({
