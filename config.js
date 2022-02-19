6e642dc5f0edf59962e2e3be46de7a95b9145d8d
shobha k <shobha9250@gmail.com>
2022-02-19 23:41:11 +0530

    port changed

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
       host: process.env.DATABASE_HOST,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
-      port: process.env.PORT,
+      port: process.env.DATABASE_PORT,
       database:process.env.DATABASE,
     },
     jwt: {
